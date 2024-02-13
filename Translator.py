4be38deeef874634c50bd5d57f3a97aab49e3bd6
Jishnu Suresh <109917899+Jishnu-JSK@users.noreply.github.com>
2024-02-13 20:57:40 +0530

    Update Translator.py
    Added few changes and done few tweaks in the code

--- Translator.py
+++ Translator.py
@@ -2,11 +2,16 @@ def translate():
     while True:
         from translate import Translator
         import time as t
-        text=input("Enter word to be translated:")
+        text=input("\nEnter word to be translated:")
         fr=input("Which is the language of the word inputed:")
         rs=input("Which language is the word be translated:")
         translator= Translator(from_lang=fr,to_lang=rs)
         translation = translator.translate(text)
+        print('''
+OUTPUT:''')
         print(translation)
         t.sleep(2)
-translate()
+        x=input("\nContinue? [Y/N]:")
+        if x in 'Nn':
+            break
+translate() 
