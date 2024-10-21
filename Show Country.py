16ce435a0cb7d9de12b2dcea47b105d53ddbd736
Jishnu Suresh <109917899+Jishnu-JSK@users.noreply.github.com>
2024-10-21 21:16:20 +0530

    Add files via upload
    !!For this program, the module plotly has to be installed!!
    To install: pip install ploty

---
+++ Show Country.py
@@ -0,0 +1,14 @@
+def sc():
+    import plotly.express as px
+    country=input("Enter your country name:")
+    data={'Country':[country],
+          'Values':[100]}
+    fig=px.choropleth(
+        data,
+        locations='Country',
+        locationmode='country names',
+        color='Values',
+        color_continuous_scale='Inferno',
+        title=f'Country Map Highlighting {country}')
+    fig.show()
+sc() 
